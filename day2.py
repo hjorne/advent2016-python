23659b479ebbaae00fd07b2ecf26fef6cab3dcc6
hjorne <joeh19@gmail.com>
2016-12-01 22:09:41 -0800

    Removed debug statement

--- day2.py
+++ day2.py
@@ -5,8 +5,8 @@ with open('day2_input.txt') as day2_input:
         instructions.append(line.strip())
 
 dirs = {'U': (0, -1), 'D': (0, 1), 'L': (-1, 0), 'R': (1, 0)}
-x, y = 1, 1
 
+x, y = 1, 1
 # Transposed from the keypad in the problem to keep (x, y) indexing the same
 keypad = ['147', '258', '369']
 combination = ''
@@ -23,10 +23,10 @@ print 'The combination to the first lock is: {0}'.format(combination)
 
 
 """ Part 2 """
-keypad = ['__5__', '_26A_', '137BD', '_48C_', '__9__']
-
 x, y = 0, 2
+keypad = ['__5__', '_26A_', '137BD', '_48C_', '__9__']
 combination = ''
+
 for line in instructions:
     for c in line:
         dx, dy = dirs[c]
@@ -34,7 +34,6 @@ for line in instructions:
         y_n = min(y + dy, 4) if y > 2 else max(y + dy, 0)
 
         # Ensures the same logic can be kept as before with a diamond keypad
-        print x_n, y_n
         if keypad[x_n][y_n] != '_':
             x, y = x_n, y_n
     combination += keypad[x][y]
