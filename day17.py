961694f657a2c9ce18d3168e28c8532388ad6c7c
hjorne <joeh19@gmail.com>
2016-12-19 21:01:47 -0800

    Added comments

--- day17.py
+++ day17.py
@@ -1,12 +1,14 @@
 from hashlib import md5
 from collections import deque
 
+# Dicts/sets for easy referencing of common operations
 i_to_dirs = {0: 'U', 1: 'D', 2: 'L', 3: 'R'}
 dirs_to_pos = {'U': (0, -1), 'D': (0, 1), 'L': (-1, 0), 'R': (1, 0)}
 open = {'b', 'c', 'd', 'e', 'f'}
 
-passcode = 'ulqzkmiv'
+passcode = 'veumntbg'
 paths = set()
+# deque is significantly faster at pop(0) than python lists
 queue = deque()
 queue.append((0, 0, ''))
 
@@ -14,9 +16,12 @@ max_pathlen = 0
 first_path = True
 while queue:
     x, y, path = queue.popleft()
-
     md5_hash = md5(passcode + path).hexdigest()[:4]
+
+    # Gets all possible movable directions
     dirs = [i_to_dirs[i] for i, c in enumerate(md5_hash) if c in open]
+
+    # Convert those directions into dx dy vals
     pos = [dirs_to_pos[d] for d in dirs]
     for d, (dx, dy) in zip(dirs, pos):
         if x + dx == 3 and y + dy == 3:
@@ -25,6 +30,7 @@ while queue:
                 first_path = False
             elif len(path + d) > max_pathlen:
                 max_pathlen = len(path + d)
+        # Ensure no walls are hit
         elif 0 <= x + dx <= 3 and 0 <= y + dy <= 3:
             queue.append((x + dx, y + dy, path + d))
 
--- day18.py
+++ day18.py
@@ -1,11 +1,15 @@
 n = 400
+
+# Adds safe vals on either end for ease of computation, ignores when counting
 with open('day18_input.txt') as day18_input:
     rows = ['.' + day18_input.readline().strip() + '.']
 
+# Much easier to work with bools than chars (True = '.' False = '^')
 rows[0] = [c == '.' for c in rows[0]]
 for i in range(n - 1):
     new_row = [True] * len(rows[i])
     for j in range(1, len(rows[i]) - 1):
+        # Straightforward implementation of restrictions in problem description
         if (not rows[i][j-1] and not rows[i][j] and rows[i][j+1] or
                 not rows[i][j] and not rows[i][j+1] and rows[i][j-1] or
                 not rows[i][j-1] and rows[i][j] and rows[i][j+1] or
@@ -13,4 +17,5 @@ for i in range(n - 1):
             new_row[j] = False
     rows.append(new_row)
 
+# True == 1, False == 0
 print sum((sum(r[1:-1]) for r in rows))
